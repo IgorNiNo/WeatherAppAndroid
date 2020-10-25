55186889958663fc3a94a5c90f464c5b5290137f
IgorNiNo <vis_74@mail.ru>
2020-10-25 18:15:39 +0300

    developLesson1

--- app/src/main/java/ru/igor_nn/myweatherapp/MainActivity.java
+++ app/src/main/java/ru/igor_nn/myweatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package ru.igor_nn.myweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView currentCity = findViewById(R.id.city);
+        String currCity = getResources().getString(R.string.city);
+//        currCity += "!!";
+        currentCity.setText(currCity);
+
+        TextView currentTemp = findViewById(R.id.unitsTemp);
+        String currTemp = "10" + " " + getResources().getString(R.string.unitsTemp);    //планирую получать текущую температуру
+//        currTemp = "10 " + currTemp;
+        currentTemp.setText(currTemp);
+
+        TextView minTemperature = findViewById(R.id.minTemp);
+        String minTemp = getResources().getString(R.string.minTemp) + " " + "-1" + " " + getResources().getString(R.string.unitsTemp);  //планирую получать температуру ночью
+        minTemperature.setText(minTemp);
+
+        TextView maxTemperature = findViewById(R.id.maxTemp);
+        String maxTemp = getResources().getString(R.string.maxTemp) + " " + "14" + " " + getResources().getString(R.string.unitsTemp);  //планирую получать температуру днём
+        maxTemperature.setText(maxTemp);
+
+        TextView realFeelTemperature = findViewById(R.id.realFeelTemp);
+        String realFeelTemp = getResources().getString(R.string.realFeelTemp) + " " + "11" + " " + getResources().getString(R.string.unitsTemp);    //планирую получать ощущаемую температуру
+        realFeelTemperature.setText(realFeelTemp);
+
     }
 }
