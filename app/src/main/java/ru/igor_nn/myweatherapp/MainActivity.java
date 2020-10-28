de26ca9085fa0664aecdfe14f3ca9cd117bcf55a
IgorNiNo <vis_74@mail.ru>
2020-10-28 21:13:10 +0300

    developLesson1_2

--- app/src/main/java/ru/igor_nn/myweatherapp/MainActivity.java
+++ app/src/main/java/ru/igor_nn/myweatherapp/MainActivity.java
@@ -12,27 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView currentCity = findViewById(R.id.city);
-        String currCity = getResources().getString(R.string.city);
-//        currCity += "!!";
-        currentCity.setText(currCity);
-
-        TextView currentTemp = findViewById(R.id.unitsTemp);
-        String currTemp = "10" + " " + getResources().getString(R.string.unitsTemp);    //планирую получать текущую температуру
-//        currTemp = "10 " + currTemp;
-        currentTemp.setText(currTemp);
-
-        TextView minTemperature = findViewById(R.id.minTemp);
-        String minTemp = getResources().getString(R.string.minTemp) + " " + "-1" + " " + getResources().getString(R.string.unitsTemp);  //планирую получать температуру ночью
-        minTemperature.setText(minTemp);
-
-        TextView maxTemperature = findViewById(R.id.maxTemp);
-        String maxTemp = getResources().getString(R.string.maxTemp) + " " + "14" + " " + getResources().getString(R.string.unitsTemp);  //планирую получать температуру днём
-        maxTemperature.setText(maxTemp);
-
-        TextView realFeelTemperature = findViewById(R.id.realFeelTemp);
-        String realFeelTemp = getResources().getString(R.string.realFeelTemp) + " " + "11" + " " + getResources().getString(R.string.unitsTemp);    //планирую получать ощущаемую температуру
-        realFeelTemperature.setText(realFeelTemp);
-
     }
 }
